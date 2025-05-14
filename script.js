e4c4c6bc60ed267a336db9430ac1302d4f88f64c
bcbke <160100667+bcbke@users.noreply.github.com>
2025-05-14 10:27:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tabs li');
+  const contents = document.querySelectorAll('.tab-content');
+
+  tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+      contents.forEach(c => c.classList.remove('active'));
+      tabs.forEach(t => t.classList.remove('active'));
+      document.getElementById(tab.dataset.tab).classList.add('active');
+      tab.classList.add('active');
+    });
+  });
+});
 
